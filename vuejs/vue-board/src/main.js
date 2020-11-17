6ad24960f374db824c3518db5a542ee8d052875d
NICEMANEUL <cndtjq22@gmail.com>
2020-11-17 17:04:49 +0900

    vue-board update

--- vuejs/vue-board/src/main.js
+++ vuejs/vue-board/src/main.js
@@ -1,12 +1,10 @@
-import Vue from "vue";
-import App from "./App.vue";
-import router from "./router";
-import store from "./store";
+import Vue from 'vue';
+import { router } from './routes/index.js';
+import App from './App.vue';
 
 Vue.config.productionTip = false;
 
 new Vue({
+  render: (h) => h(App),
   router,
-  store,
-  render: h => h(App)
-}).$mount("#app");
+}).$mount('#app');
