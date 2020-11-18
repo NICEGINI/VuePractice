eed5944bfdc67110ba808387573359839990a205
YSM <cndtjq145@naver.com>
2020-11-18 09:38:02 +0900

    update

--- vuejs/HW04/router.js
+++ vuejs/HW04/router.js
@@ -5,27 +5,27 @@ import Index from './component/Index.js';
 
 Vue.use(VueRouter);
 export default new VueRouter({
-  mode: 'history',
-  routes: [
-    {
-      path: '/',
-      name: 'index',
-      component: Index,
-    },
-    {
-      path: '/list',
-      name: 'list',
-      component: List,
-    },
-    {
-      path: '/add',
-      name: 'add',
-      component: Add,
-    },
-    {
-      path: '/detail',
-      name: 'detail',
-      component: Detail,
-    },
-  ],
+    mode: 'history',
+    routes: [
+        {
+            path: '/',
+            name: 'index',
+            component: Index,
+        },
+        {
+            path: '/list',
+            name: 'list',
+            component: List,
+        },
+        {
+            path: '/add',
+            name: 'add',
+            component: Add,
+        },
+        {
+            path: '/detail',
+            name: 'detail',
+            component: Detail,
+        },
+    ],
 });
