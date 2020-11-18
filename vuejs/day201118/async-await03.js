d8ad6242e004d9916ff81d72433e257db1599031
YSM <cndtjq145@naver.com>
2020-11-18 11:17:35 +0900

    async와 await 사용법 연습

---
+++ vuejs/day201118/async-await03.js
@@ -0,0 +1,40 @@
+/**
+ * https://developer.mozilla.org/ko/docs/Web/JavaScript/Reference/Statements/async_function
+ *
+ * await 사용 방법
+ * await 키워드 뒤에는 Promise 객체가 와야한다
+ * await 키워드를 사용하기 위해서는  async함수와 함께 해야한다!
+ * await는 성공한 경우에만 받을 수 있다.
+ * reject인 경우에는 에러 처리기능이 없기 때문에 에러가 발생한다.
+ * 그래서 따로 예외처리를 해주어야 한다.
+ */
+
+function axios(url) {
+    return new Promise((resolve, reject) => {
+        setTimeout(() => {
+            resolve('결과 처리 성공');
+        }, 2000);
+    });
+}
+
+async function callAxios() {
+    console.log('1');
+    axios('http://localhost:9999/board').then((result) => {
+        console.log(result);
+    });
+    console.log('2');
+
+    // 아래 await 키워드부터 axios 끝날때까지 멈춤.
+    try {
+        let result = await axios('http://localhost:9999/board');
+        console.log('processing axios start');
+        console.log('await: ', result);
+        console.log('processing axios end');
+    } catch {
+        console.log('reject인 경우!');
+    }
+}
+
+console.log('callAxios before');
+callAxios();
+console.log('callAxios after');
